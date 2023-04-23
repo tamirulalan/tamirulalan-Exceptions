9ed5e8e6622fb6416deb24b9663f00a4faaa54e7
Tamiru Taye <tamirulalan@gmail.com>
2023-04-23 19:01:04 +0000

    Exception

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -19,8 +19,9 @@ public class Lab {
      * a method thrown to it. A try/catch block would handle the exception within the method.
      *
      * Check out this guide on throwing exceptions: https://rollbar.com/guides/java/how-to-throw-exceptions-in-java/#
+     * @throws Exception
      */
-    public void mustThrow() {
-
+    public void mustThrow() throws Exception {
+        throw new Exception("Exception message");
     }
 }
